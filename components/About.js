3b26adfd26045c92eb043a0457b2e5aba0cfab7e
Helvin <SD1.Helvin@infra.core>
2026-03-17 20:43:49 +0700

    style(ui): align the about us section to the center

--- components/About.js
+++ components/About.js
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <section id="about" className="py-24 px-10 max-w-4xl mx-auto transition-colors duration-500">
+    <section id="about" className="py-24 px-10 max-w-4xl mx-auto transition-colors text-center duration-500">
       <h2 className="text-3xl font-bold mb-6">About Me</h2>
 
       <p className="text-gray-700 leading-relaxed">
